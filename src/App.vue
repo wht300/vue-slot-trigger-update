21328467f76a8a8687d17e0b598418476ccfeabd
HengtaiWang <hengtai.wang@cummins.com>
2023-04-24 17:58:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <LayoutWrap>
+    on slot
+    <template v-for="value in list" :key="value">
+      <div v-show="index===count"  :ref="refs.set">{{ value }}</div>
+    </template>
+    <div>refs.length:{{ refs.length }}</div>
+  </LayoutWrap>
+  <br>
+  <div>
+    on div
+    <template v-for="value in list" :key="value">
+      <div v-show="index===count"  :ref="refs.set">{{ value }}</div>
+    </template>
+    <div>refs2.length:{{ refs2.length }}</div>
+  </div>
+  <div>
+    <button @click="addCound">+1</button>
+    {{ count }}
+    <button @click="subCound">-1</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import { useTemplateRefsList } from '@vueuse/core';
+
+// components
+import LayoutWrap from '@/components/LayoutWrap.vue';
+
+export default defineComponent({
+  name: 'BaseOrder',
+  components: {
+    LayoutWrap,
+  },
+  setup() {
+    let count = ref(1);
+    const list=ref([1,2,3])
+    const refs = useTemplateRefsList();
+    const refs2 = useTemplateRefsList();
+    const addCound=()=>{
+      count.value++
+    }
+    // 减一
+    const subCound=()=>{
+      count.value--
+    }
+    return {
+      count,
+      list,
+      refs,
+      refs2,
+      addCound,
+      subCound
+    };
+  },
+});
+</script>
